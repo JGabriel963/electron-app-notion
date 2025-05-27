bfdb0abbfe56dce19045f694c40389e160c7a8b4
João Gabriel <joaogabriel9633@gmail.com>
2025-05-27 20:39:13 -0300

    Files

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,12 @@
+export function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the React </h1>
+      </header>
+      <main>
+        <p>This is a simple React application.</p>
+      </main>
+    </div>
+  );
+}
